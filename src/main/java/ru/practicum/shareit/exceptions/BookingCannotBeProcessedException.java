07f73918767bc56aaefbb31fb28d1c6bc1241bd8
kiril <kirill551@yandex.ru>
2024-03-15 19:38:01 +0300

    "Feature: реализовать функциональность согласно ТЗ"

--- src/main/java/ru/practicum/shareit/exceptions/BookingCannotBeProcessedException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class BookingCannotBeProcessedException extends RuntimeException {
-
-    public BookingCannotBeProcessedException(String message) {
-        super(message);
-    }
-}
